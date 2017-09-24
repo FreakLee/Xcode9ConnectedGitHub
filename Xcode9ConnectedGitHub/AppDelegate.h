e5f043f7e652d41514b1bf195a1f05fcd1119c08
Lyman <liming.swift@gmail.com>
2017-09-24 20:34:39 +0800

    Initial Commit

---
+++ Xcode9ConnectedGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xcode9ConnectedGitHub
+//
+//  Created by Li Ming  on 2017/9/24.
+//  Copyright © 2017年 Lyman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Xcode9ConnectedGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xcode9ConnectedGitHub
+//
+//  Created by Li Ming  on 2017/9/24.
+//  Copyright © 2017年 Lyman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
